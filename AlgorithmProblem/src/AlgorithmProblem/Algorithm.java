4e757e72b0d9deca47147f778ecc562439fda552
John Crinkey <32498739+crinkj@users.noreply.github.com>
2020-06-12 15:27:33 +0900

    algorithm problems 3

---
+++ AlgorithmProblem/src/AlgorithmProblem/Algorithm.java
@@ -0,0 +1,46 @@
+package AlgorithmProblem;
+
+import java.util.Scanner;
+
+public class Algorithm {
+	
+	public static void main(String args[]) {
+
+// 기초 알고리즘 100문제(Code Up)
+		
+// * Problem 1013 
+//		Problem: 2개의 정수가 공백으로 구분되어 입력된다.
+		
+//		    Scanner num = new Scanner(System.in);
+//		    int num1 = num.nextInt();
+//		    int num2 = num.nextInt();
+//		        
+//		       System.out.println(num1 + " " + num2);
+//	    	   input: 1 2  output: 1 2
+		
+// * Problem 1014
+//		Problem: 2개의 문자(ASCII CODE)를 입력받아서 순서를 바꿔 출력해보자.
+		
+//			Scanner stg = new Scanner(System.in);		
+//			String fst = stg.next();
+//			String snd = stg.next();
+//			
+//			System.out.println(snd + " " + fst);
+ //         input: A b  output: b A
+
+// * Problem 1017
+// 		Problem: int형 정수 1개를 입력받아 공백을 사이에 두고 3번 출력해보자.
+		
+//			Scanner sc = new Scanner(System.in);
+//			int num = sc.nextInt();
+//			
+//				for(int i=0; i<3; i++) {
+//					System.out.print(num + " ");
+//				}
+//           input: 125 output: 125 125 125
+	
+	
+		
+	}	
+	
+}

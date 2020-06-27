b6d99f62842e0cf5bdc6bc05ce98d37aa5de83cd
John Crinkey <32498739+crinkj@users.noreply.github.com>
2020-06-27 20:33:45 +0900

    Algo Practice

--- AlgorithmProblem/src/AlgorithmProblem/Algorithm.java
+++ AlgorithmProblem/src/AlgorithmProblem/Algorithm.java
@@ -632,8 +632,30 @@ public class Algorithm {
 //        input:  10							output: 5 8 9 7 6 6 3 2 4 10
 //        		10 4 2 3 6 6 7 9 8 5
         
+//	* Problem 1095
+//		* Problem: 번호를 부른 횟수(n, 1 ~ 10000)가 첫 줄에 입력된다. n개의 랜덤 번호(k, 1 ~ 23)가 두 번째 줄에 공백을 사이에 두고 순서대로 입력된다.
+//		(출석을 부른 번호 중에 가장 빠른 번호를 1개만 출력한다.)
+//		Scanner sc = new Scanner(System.in);
+//        
+//        int num = sc.nextInt();
+//        
+//        int[] arr = new int[num];    
+//        for(int i =0 ; i < num; i++){
+//            int att = sc.nextInt();
+//            arr[i] = att;
+//        }
+//        
+//        int first = arr[0];
+//        for(int i=0; i<arr.length; i++){
+//               
+//        	if(first >= arr[i]) {
+//        		first = arr[i];
+//        	}
+//        }
+//        System.out.println(first);
         
-        
+//		input: 10							output: 2
+//			   10 4 2 3 6 6 7 9 8 5
         
         
         

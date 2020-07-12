5ec064bceb8323653ff1af7b3e6044f0042545e1
John Crinkey <32498739+crinkj@users.noreply.github.com>
2020-07-12 21:12:55 +0900

    Updated

--- AlgorithmProblem/src/Baekjoon/Greedy_Problems.java
+++ AlgorithmProblem/src/Baekjoon/Greedy_Problems.java
@@ -1,5 +1,8 @@
 package Baekjoon;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Greedy_Problems{
     public static void main(String[] args){
         
@@ -84,6 +87,8 @@ public class Greedy_Problems{
 //          System.out.println(count);
 //          
 //          input: 380			output: 4  // 620원  500원 1개 100원 1개 십원 2
+
+
 //      * Problem 2217: 첫째 줄에 정수 N이 주어진다. 다음 N개의 줄에는 각 로프가 버틸 수 있는 최대 중량이 주어진다. 이 값은 10,000을 넘지 않는 자연수이다.
 //    	  
 //    	   Scanner sc = new Scanner(System.in);
@@ -104,5 +109,63 @@ public class Greedy_Problems{
 //		    
 //		    input: 2 			output: 30
 //		    	   15 20    
-    }    
+    	
+//		* Problem 10610: 어느 날, 미르코는 우연히 길거리에서 양수 N을 보았다. 미르코는 30이란 수를 존경하기 때문에, 그는 길거리에서 찾은 수에 포함된 숫자들을 섞어 30의 배수가 되는 가장 큰 수를 만들고 싶어한다.
+//  							미르코를 도와 그가 만들고 싶어하는 수를 계산하는 프로그램을 작성하라. 만약 그숫자가안만들어지면 -1 출	
+//		문제조건 받은 숫자를 정해서 섞어서 30배수가 되는 가장 큰 숫자를 만들려면
+//					마지막은 0으로 끝나면서 다 더했을때 3으로 나누면 0으로떨어지는 숫자여야한다 
+//    	Scanner sc = new Scanner(System.in);
+//    	
+//    	String num = sc.next();
+//    	int[] arr = new int[num.length()];
+//    	int sum = 0; 
+//    	
+//    	for(int i=0; i<arr.length; i++) {
+//    		arr[i] = num.charAt(i) - '0'; // change string character to int
+//    		
+//    		sum += arr[i]; // declare sum if it can be divided by 3.
+//    	}
+//    	Arrays.sort(arr);	// sort an array from smallest number to largest number
+//    	if(sum % 3 == 0 && arr[0] == 0) {
+//    		for(int i = arr.length -1; i >= 0; i--) {
+//    			System.out.print(arr[i] + "");
+//    		}
+//    	}
+//    		else {
+//    			System.out.println(-1);
+//    		
+//    	}
+//     
+//    	input: 102			output: 210
+//    	input: 2931			output: -1
+    
+//    	* Problem 1541: 첫째 줄에 식이 주어진다. 식은 ‘0’~‘9’, ‘+’, 그리고 ‘-’만으로 이루어져 있고, 가장 처음과 마지막 문자는 숫자이다. 그리고 연속해서 두 개 이상의 연산자가 나타나지 않고, 5자리보다 많이 연속되는 숫자는 없다. 수는 0으로 시작할 수 있다.
+//         조건: -로시작하는 자리 뒤에 (를 추가하고 새로은 -로시작하기전에 )를 추가한다
+    
+//    	Scanner sc = new Scanner(System.in);
+//    	
+//    	String num = sc.nextLine();
+//    	
+//    	for(int i=0; i< num.length(); i++){
+//    		String[] arr = num.split("");
+//    		 
+//    		if(arr[i] == "-") {
+//    			arr[i] = "-(";
+//    		};
+//    		
+//    	}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+    
+    
+    
 }

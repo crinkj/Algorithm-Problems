d7689c1470744cd174d5a23942f833b455bbe739
이요한 <jcrinkey89120@gmail.com>
2021-06-03 22:23:08 +0900

    Working on problem10

---
+++ AlgorithmProblem/src/inflearn_Algorithm/problem10.java
@@ -0,0 +1,22 @@
+package inflearn_Algorithm;
+
+import java.util.Scanner;
+
+public class problem10 {
+	public int[] solution(String s, char t) {
+		int[] answer = new int[s.length()];
+		int p=1000;
+		for(jnt i=0; i<s.length(); i++) {
+			if(s.charAt(i))
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		problem10 prob = new problem10();
+		Scanner sc = new Scanner(System.in);
+		String str = new
+
+	}
+}

3a4ad5f709d9b5ee6fed51ac83334a5f6d476dd2
이요한 <jcrinkey89120@gmail.com>
2021-06-04 21:42:36 +0900

    problem_10 is finished

--- AlgorithmProblem/src/inflearn_Algorithm/problem10.java
+++ AlgorithmProblem/src/inflearn_Algorithm/problem10.java
@@ -6,8 +6,15 @@ public class problem10 {
 	public int[] solution(String s, char t) {
 		int[] answer = new int[s.length()];
 		int p=1000;
-		for(jnt i=0; i<s.length(); i++) {
-			if(s.charAt(i))
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)==t) {
+				p=0;
+				answer[i]=p;
+			}
+			else {
+				p++;
+				answer[i]=p;
+			}
 		}
 
 		return answer;
@@ -16,7 +23,11 @@ public class problem10 {
 	public static void main(String[] args) {
 		problem10 prob = new problem10();
 		Scanner sc = new Scanner(System.in);
-		String str = new
+		String str = sc.next();
+		char c = sc.next().charAt(0);
+		for(int x  : prob.solution(str, c)) {
+			System.out.println(x + " ");
+		}
 
 	}
 }

89df2bf331dd21146df16587277c05571c54f84e
이요한 <jcrinkey89120@gmail.com>
2021-06-07 23:19:43 +0900

    baekjoon_2839 is added

--- AlgorithmProblem/src/Baekjoon/baekjoon_11497.java
+++ AlgorithmProblem/src/Baekjoon/baekjoon_11497.java
@@ -1,4 +1,4 @@
-package inflearn_Algorithm;
+package Baekjoon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- AlgorithmProblem/src/Baekjoon/baekjoon_11508.java
+++ AlgorithmProblem/src/Baekjoon/baekjoon_11508.java
@@ -1,4 +1,4 @@
-package inflearn_Algorithm;
+package Baekjoon;
 
 import java.util.Arrays;
 import java.util.Comparator;

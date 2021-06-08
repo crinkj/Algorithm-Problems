ea04acd911df46370253659986c55ccca797a862
이요한 <jcrinkey89120@gmail.com>
2021-06-08 23:19:06 +0900

    modifying problem10.

--- AlgorithmProblem/src/inflearn_Algorithm/problem10.java
+++ AlgorithmProblem/src/inflearn_Algorithm/problem10.java
@@ -3,31 +3,24 @@ package inflearn_Algorithm;
 import java.util.Scanner;
 
 public class problem10 {
-	public int[] solution(String s, char t) {
-		int[] answer = new int[s.length()];
-		int p=1000;
-		for(int i=0; i<s.length(); i++) {
-			if(s.charAt(i)==t) {
-				p=0;
-				answer[i]=p;
-			}
-			else {
-				p++;
-				answer[i]=p;
-			}
-		}
+
+// 문제: 한 개의 문자열 s와 문자 t가 주어지면 문자열 s의 각 문자가 문자 t와 떨어진 최소거리를 출력하는 프로그램을 작성하세요.
+	public static void main(String[] args) {
+		problem10 prob = new problem10();
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		char letter = sc.next().charAt(0);
+		System.out.println(prob.solution(word,letter));
+
+	}
+
+	public String solution(String word, char letter) {
+		String answer = "";
+		char[] arr = new char[word.length()];
+		arr = word.toCharArray();
 
 		return answer;
 	}
 
-	public static void main(String[] args) {
-		problem10 prob = new problem10();
-		Scanner sc = new Scanner(System.in);
-		String str = sc.next();
-		char c = sc.next().charAt(0);
-		for(int x  : prob.solution(str, c)) {
-			System.out.println(x + " ");
-		}
 
-	}
 }

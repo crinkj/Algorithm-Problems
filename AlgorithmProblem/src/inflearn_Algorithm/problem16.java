e696f234a8e0a10b29bb9468012ed707490e2c39
이요한 <jcrinkey89120@gmail.com>
2021-06-17 23:09:12 +0900

    working on problem17 and not done yet.

--- AlgorithmProblem/src/inflearn_Algorithm/problem16.java
+++ AlgorithmProblem/src/inflearn_Algorithm/problem16.java
@@ -3,6 +3,9 @@ package inflearn_Algorithm;
 import java.util.Scanner;
 
 public class problem16 {
+//	1) 피보나키 수열을 출력한다. 피보나치 수열이란 앞의 2개의 수를 합하여 다음 숫자가 되는 수열이다.
+//
+//	2) 입력은 피보나치 수열의 총 항의 수 이다. 만약 7이 입력되면 1 1 2 3 5 8 13을 출력하면 된다.
 	public static void main(String[] args) {
 		problem16 prob = new problem16();
 
@@ -24,11 +27,17 @@ public class problem16 {
 		arr[0] = 1;
 		arr[1] = 1;
 		for(int i=2; i<num;i++) {
-			c=a+b;
-			arr[i]= c;
-			a=b;
-			b=c;
+			arr[i] = arr[i-2] + arr[i-1];
 		 }
 		return arr;
 	}
+	// 손코딩(배열 쓰지않고)
+//	public void solution(int n) {
+//		int a=1; b=1,c;
+//		for(int i=2; i<n; i++) {
+//			c=a+b;
+//			a=b;
+//			b=c;
+//		}
+//	}
 }
---
+++ AlgorithmProblem/src/inflearn_Algorithm/problem17.java
@@ -0,0 +1,22 @@
+package inflearn_Algorithm;
+
+import java.util.Scanner;
+
+public class problem17 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+
+		int count = 0;
+
+		for(int i=2; i<num; i++) {
+			if(i % num == 0) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}

8dfa407ece9fdf605547297172eeecd459d0337e
이요한 <jcrinkey89120@gmail.com>
2021-06-18 20:34:24 +0900

    problem 17 is solved.

--- AlgorithmProblem/src/inflearn_Algorithm/problem17.java
+++ AlgorithmProblem/src/inflearn_Algorithm/problem17.java
@@ -8,12 +8,16 @@ public class problem17 {
 		Scanner sc = new Scanner(System.in);
 
 		int num = sc.nextInt();
+		int[] ch = new int[num+1];
 
 		int count = 0;
 
-		for(int i=2; i<num; i++) {
-			if(i % num == 0) {
+		for(int i=2;i<=num; i++) {
+			if(ch[i] == 0) {
 				count++;
+				for(int j=i;j<=num;j=j+i) {
+					ch[j] = 1;
+				}
 			}
 		}
 		System.out.println(count);

4fa7c0f524cae51968640e8746be37a493ce67e8
이요한 <jcrinkey89120@gmail.com>
2021-07-11 19:51:26 +0900

    programmer problem is added

---
+++ AlgorithmProblem/src/Programmers/middleCharacterInString.java
@@ -0,0 +1,19 @@
+package Programmers;
+
+public class middleCharacterInString {
+/*
+ * 문제: 	단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+ */
+	public String solution(String s) {
+        String answer = "";
+        char[] arr = s.toCharArray();
+
+        if(arr.length % 2 == 1){ // 캐릭터 배열이 홀 수 일경우 
+            answer += arr[arr.length/2];  
+        }else{ // 캐릭터 배열이 짝우리경우
+           answer += arr[(arr.length/2)-1];
+           answer += arr[(arr.length/2)];  //
+        }
+        return answer;
+    }
+}

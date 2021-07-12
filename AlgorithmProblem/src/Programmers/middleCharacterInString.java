8dbd7b1a6bde54539e6d1a1a610598d89f4a10e5
이요한 <jcrinkey89120@gmail.com>
2021-07-12 21:47:01 +0900

    programmer problem: https://programmers.co.kr/learn/courses/30/lessons/1845 is added.

--- AlgorithmProblem/src/Programmers/middleCharacterInString.java
+++ AlgorithmProblem/src/Programmers/middleCharacterInString.java
@@ -2,7 +2,7 @@ package Programmers;
 
 public class middleCharacterInString {
 /*
- * 문제: 	단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+ * 문제: 	가운데 글자 가져오기)단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
  */
 	public String solution(String s) {
         String answer = "";

ff105a50fe73649e0bc7d83d8bb762e320345a8b
이요한 <jcrinkey89120@gmail.com>
2021-07-15 23:32:11 +0900

    working on numStringArray and finished noSameNum

---
+++ AlgorithmProblem/src/Programmers/numStringArray.java
@@ -0,0 +1,17 @@
+package Programmers;
+
+public class numStringArray {
+/*
+ * https://programmers.co.kr/learn/courses/30/lessons/81301
+ */
+	public static void main(String[] args) {
+		String s = "one4seveneight";
+		String[] arr = {"one","two","three","four","five","six","seven","eight","nine"};
+		char[] prob = s.toCharArray();
+		String answer = "";
+		if(s.contains("one")) {
+			answer += "1";
+		}
+	}
+
+}

053839bbe27ed00f2b030a3e5a14dca4fd28b711
이요한 <jcrinkey89120@gmail.com>
2021-07-19 23:50:32 +0900

    problem(budget) is solved.

--- AlgorithmProblem/src/Programmers/budget.java
+++ AlgorithmProblem/src/Programmers/budget.java
@@ -7,8 +7,8 @@ public class budget {
  * https://programmers.co.kr/learn/courses/30/lessons/12982
  */
 	public static void main(String[] args) {
-		int[] d = {1,3,2,5,4};
-		int budget = 9;
+		int[] d = {2,2,3,3};
+		int budget = 10;
 
 		System.out.println(solution(d,budget));
 	}
@@ -17,10 +17,9 @@ public class budget {
 		int answer = 0;
 
 		Arrays.sort(d);
-		int change = 0;
 		for(int i=0;i<d.length;i++) {
-			change = budget % d[i];
-			if(change >= 0) {
+			budget = budget - d[i];
+			if(budget > 0 || budget == 0) {
 				answer++;
 			}
 		}
